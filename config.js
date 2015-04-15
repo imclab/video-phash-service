6c154dbf02c82e7f67996819fcad1580b796889e
Jonathan Ong <jonathanrichardong@gmail.com>
2015-04-14 17:57:23 -0700

    add experimental heroku worker support

--- config.js
+++ config.js
@@ -2,3 +2,5 @@
 exports.redis_uri = process.env.REDIS_URI || 'tcp://localhost:6379'
 exports.password = process.env.PASSWORD || 'mgmt'
 exports.maxage = parseInt(process.env.MAX_AGE) || 30 * 24 * 60 * 60
+exports.heroku_api_token = process.env.HEROKU_API_TOKEN
+exports.heroku_app = process.env.HEROKU_APP
